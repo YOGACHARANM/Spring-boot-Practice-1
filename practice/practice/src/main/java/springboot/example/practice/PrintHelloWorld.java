1c70217a116e197a43c895fd7b7e0f2572c8d511
Yogacharan M <165685666+YOGACHARANM@users.noreply.github.com>
2026-04-24 10:50:49 +0530

    Fix RestController annotation

--- practice/practice/src/main/java/springboot/example/practice/PrintHelloWorld.java
+++ practice/practice/src/main/java/springboot/example/practice/PrintHelloWorld.java
@@ -3,7 +3,9 @@ package springboot.example.practice;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.RestController;
+@controller
+    @RestMapping
 
     @RestController
     public class PrintHelloWorld {
